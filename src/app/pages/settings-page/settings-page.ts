4ea2c5b5a1ecf6bbaf701c21e51cefd7fc8134ea
JudeMichaelArriba <arribajudemichael@gmail.com>
2026-03-18 01:02:31 +0800

    feat: Added functionalities for updating the user fullname using user services

--- src/app/pages/settings-page/settings-page.ts
+++ src/app/pages/settings-page/settings-page.ts
@@ -3,7 +3,9 @@ import { CommonModule } from '@angular/common';
 import { AuthStateService } from '../../services/auth-state.service';
 import { User } from '../../models/user.model';
 import { Observable } from 'rxjs';
-
+import { UserService } from '../../services/user';
+import { LoggerService } from '../../services/logger.service';
+import { DialogService } from '../../services/dialog.service';
 
 @Component({
   selector: 'app-settings-page',
@@ -17,8 +19,14 @@ export class SettingsPage {
   currentUser$: Observable<User | null>;
   isEditing = false;
   fullNameDraft = '';
+  isSaving = false;
 
-  constructor(private auhtState: AuthStateService) {
+  constructor(
+    private auhtState: AuthStateService,
+    private userService: UserService,
+    private logger: LoggerService,
+    private dialogService: DialogService
+  ) {
     this.currentUser$ = this.auhtState.currentUser$;
   }
 
@@ -30,6 +38,7 @@ export class SettingsPage {
 
 
   startEdit(userFullname: string | null | undefined) {
+    if (this.isSaving) return;
     this.fullNameDraft = userFullname ?? "";
     this.isEditing = true;
   }
@@ -38,7 +47,44 @@ export class SettingsPage {
     this.isEditing = false;
   }
 
-  async saveChanges() {
-    this.isEditing = false;
+  async saveChanges(user: User | null) {
+
+    if (!user) return;
+    const trimmed = this.fullNameDraft.trim();
+    const current = user.fullName?.trim() ?? '';
+
+
+    if (trimmed === current) {
+      this.dialogService.alert('No Changes', 'You have not made any changes to save.');
+      return;
+    }
+
+    if (!trimmed) {
+      this.dialogService.error('Validation Error', 'Full name cannot be empty.');
+      return;
+    }
+
+    this.dialogService.confirm(
+      'Confirm update',
+      'Save your updated full name?',
+      async () => {
+
+        this.isSaving = true;
+        try {
+          await this.userService.updateUserFullName(user.uid, trimmed);
+          this.auhtState.setUser({ ...user, fullName: trimmed });
+          this.isEditing = false;
+
+          this.dialogService.success('Updated', 'Your name was updated successfully.');
+        } catch (err) {
+          this.logger.error('Failed to update name', err);
+          this.dialogService.error('Update Failed', 'Something went wrong. Please try again.');
+        } finally {
+          this.isSaving = false;
+        }
+      }
+
+    );
+
   }
 }
