654c74ff0c7e4c0624ead872939d3a446c3efd16
JudeMichaelArriba <arribajudemichael@gmail.com>
2026-03-18 00:04:14 +0800

    feat: used the authstate services to display user info

--- src/app/pages/settings-page/settings-page.ts
+++ src/app/pages/settings-page/settings-page.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthStateService } from '../../services/auth-state.service';
+import { User } from '../../models/user.model';
+import { Observable } from 'rxjs';
+
+
 @Component({
   selector: 'app-settings-page',
   standalone: true,
@@ -7,6 +12,18 @@ import { CommonModule } from '@angular/common';
   templateUrl: './settings-page.html',
   styleUrl: './settings-page.css',
 })
-export class SettingsPage {
 
+export class SettingsPage {
+  currentUser$: Observable<User | null>;
+
+
+  constructor(private auhtState: AuthStateService) {
+    this.currentUser$ = this.auhtState.currentUser$;
+  }
+
+  getUserInitials(user?: User | null): string {
+    if (!user?.fullName) return 'U';
+    const parts = user.fullName.trim().split(' ');
+    return parts.map(p => p.charAt(0).toUpperCase()).slice(0, 2).join('');
+  }
 }
