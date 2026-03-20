4894b099dcfa517ecd616a6feed8c5d191abb4df
JudeMichaelArriba <arribajudemichael@gmail.com>
2026-03-20 08:54:22 +0800

    feat: added environement dev for deployment

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAMEtofOaH_miuvFxOTEEjuIxm4wzLnQDI",
+    authDomain: "ocutemp.firebaseapp.com",
+    databaseURL: "https://ocutemp-default-rtdb.firebaseio.com/",
+    projectId: "ocutemp",
+    storageBucket: "ocutemp.firebasestorage.app",
+    messagingSenderId: "700435856134",
+    appId: "1:700435856134:web:f4e893c1d80cab9bc4bc87"
+  }
+};
